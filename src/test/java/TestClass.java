9e6c722721cdca7d9229b8aa92ea338fe275386d
Dmitry Baklanov <dmitry.baklanov@mainsoft.org>
2020-09-02 13:57:12 +0300

    adding of a new Class with tests

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestClass {
+    @Test
+    public void testSuccess() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testFail() {
+        Assert.assertTrue(false);
+    }
 }
